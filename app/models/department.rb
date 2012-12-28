410dda90db4b16822d60cc2c65e3c7a08bc731db
John Baycroft <jbaycroft@capaccio.com>
2012-12-28 11:56:59 -0500

    set up tasks#index for AJAX div calls

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,4 @@
 class Department < ActiveRecord::Base
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
   attr_accessible :complete, :date_completed, :description, :location, :name
 end
