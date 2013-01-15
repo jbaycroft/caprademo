b308b8694ac6768301cd68f89afaf3cd8d66fe58
Andy Roland <aroland@capaccio.com>
2013-01-15 15:22:00 -0500

    task style sheet adjustments

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 	for (var i = 0; i < result.length; i++) {
 		var t = result[i];
 		if (t == 1) {
-			$('li.one, tr.one').removeClass('hidden');
+			$('tr.one').removeClass('hidden');
 		}
 		if (t == 2) {
-			$('li.two, tr.two').removeClass('hidden');
+			$('tr.two').removeClass('hidden');
 		}
 		if (t == 3) {
-			$('li.three, tr.three').removeClass('hidden');
+			$('tr.three').removeClass('hidden');
 		}
 		if (t == 4) {
-			$('li.four, tr.four').removeClass('hidden');
+			$('tr.four').removeClass('hidden');
 		}
 	}
 });
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ActiveRecord::Base
   belongs_to :department
   has_many :hazards, :dependent => :destroy
   attr_accessible :complete, :date_completed, :description, :hazard1, :hazard2, :hazard3, :hazard4, :location, :name
+  accepts_nested_attributes_for :hazards
 end
