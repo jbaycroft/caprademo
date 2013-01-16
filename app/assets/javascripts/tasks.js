bb670393e3dc17e1a24bc17728f03050cf81451a
Andy Roland <aroland@capaccio.com>
2013-01-16 13:05:41 -0500

    create hazard form templates

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,9 @@
+//Button Populate Javascript
+//This determines which hazard buttons are shown in the task show view based on what checkboxes are selected in the task form
 $(document).ready(function(){
-	var value = $('li#yes_no > #value, li#yes_no > #comma').text();
+	var value = $('li.yes_no > #value, li.yes_no > #comma').text();
 	var value = value.split(",");
-	var index = $('li#yes_no > #index, li#yes_no > #comma').text();
+	var index = $('li.yes_no > #index, li.yes_no > #comma').text();
 	var index = index.split(",");
 	var result = new Array ();
 	for (i = 0; i < index.length; i++) {
@@ -25,17 +27,19 @@ $(document).ready(function(){
 		}
 	}
 });
+//Hazard Form Populate Javascript
+//This determines which hazard form is shown when a hazard button is activated
 jQuery(function($){
 	function changeTab(e) {
 		e.preventDefault();
-		$('li.checkbox1 div.active').removeClass("active");
+		$('li.yes_no div.active').removeClass("active");
 		$(this).addClass("active");
 		var det = $('.active').attr('id');
 		var $$ = {
-        'ha1': $('div#haz1'),
-        'ha2': $('div#haz2'),
-        'ha3': $('div#haz3'),
-        'ha4': $('div#haz4')
+        'ha1': $('div.haz1'),
+        'ha2': $('div.haz2'),
+        'ha3': $('div.haz3'),
+        'ha4': $('div.haz4')
    		 };
 		if (det == "haz1") {
             $$.ha1.show('slow');
@@ -64,7 +68,7 @@ jQuery(function($){
             $$.ha4.hide('slow');
         }
 	}
-	$("li.checkbox1 div").click(changeTab);
+	$("li.yes_no div").click(changeTab);
 	$('div.complete').bind('click', function() {
         $('input#hazard_complete').prop("checked",true);
     });
