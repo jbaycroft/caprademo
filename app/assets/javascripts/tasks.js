b6e1fae5386730769bc274fa9e0b36cf461c02fb
John Baycroft <jbaycroft@capaccio.com>
2013-01-16 09:41:47 -0500

    Add Tab functionality to Task#show

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -30,7 +30,42 @@ jQuery(function($){
 		e.preventDefault();
 		$('li.checkbox1 div.active').removeClass("active");
 		$(this).addClass("active");
+		var det = $('.active').attr('id');
+		var $$ = {
+        'ha1': $('div#haz1'),
+        'ha2': $('div#haz2'),
+        'ha3': $('div#haz3'),
+        'ha4': $('div#haz4')
+   		 };
+		if (det == "haz1") {
+            $$.ha1.show('slow');
+            $$.ha2.hide('slow');
+            $$.ha3.hide('slow');
+            $$.ha4.hide('slow');
+        } else if (det == "haz2") {
+            $$.ha1.hide('slow');
+            $$.ha2.show('slow');
+            $$.ha3.hide('slow');
+            $$.ha4.hide('slow');
+        } else if (det == "haz3") {
+            $$.ha1.hide('slow');
+            $$.ha2.hide('slow');
+            $$.ha3.show('slow');
+            $$.ha4.hide('slow');
+        } else if (det == "haz4") {
+            $$.ha1.hide('slow');
+            $$.ha2.hide('slow');
+            $$.ha3.hide('slow');
+            $$.ha4.show('slow');
+        } else {
+            $$.ha1.hide('slow');
+            $$.ha2.hide('slow');
+            $$.ha3.hide('slow');
+            $$.ha4.hide('slow');
+        }
 	}
 	$("li.checkbox1 div").click(changeTab);
-
+	$('div.complete').bind('click', function() {
+        $('input#hazard_complete').prop("checked",true);
+    });
 });
