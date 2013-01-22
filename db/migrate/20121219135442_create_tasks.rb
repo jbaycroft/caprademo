e1e20a39b843bfef14d2f7ae85f0a57d4fcad0c0
John Baycroft <jbaycroft@capaccio.com>
2013-01-22 15:02:23 -0500

    updated tasks migration

--- db/migrate/20121219135442_create_tasks.rb
+++ db/migrate/20121219135442_create_tasks.rb
@@ -7,6 +7,22 @@ class CreateTasks < ActiveRecord::Migration
       t.boolean :hazard2
       t.boolean :hazard3
       t.boolean :hazard4
+      t.boolean :hazard5
+      t.boolean :hazard6
+      t.boolean :hazard7
+      t.boolean :hazard8
+      t.boolean :hazard9
+      t.boolean :hazard10
+      t.boolean :hazard11
+      t.boolean :hazard12
+      t.boolean :hazard13
+      t.boolean :hazard14
+      t.boolean :hazard15
+      t.boolean :hazard16
+      t.boolean :hazard17
+      t.boolean :hazard18
+      t.boolean :hazard19
+      t.boolean :hazard20
       t.string :location
       t.boolean :complete
       t.date :date_completed
