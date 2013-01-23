de730f6bb248ad0f064c4f10365b70fa0dc00a6a
John Baycroft <jbaycroft@capaccio.com>
2013-01-23 10:04:32 -0500

    added hazard, 5-20 to task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :department
   has_many :hazards, :dependent => :destroy
-  attr_accessible :complete, :date_completed, :description, :hazard1, :hazard2, :hazard3, :hazard4, :location, :name
+  attr_accessible :complete, :date_completed, :description, :hazard1, :hazard2, :hazard3, :hazard4, :hazard5, :hazard6, :hazard7,:hazard8, :hazard9, :hazard10, :hazard11, :hazard12, :hazard13, :hazard14, :hazard15, :hazard16, :hazard17, :hazard18, :hazard19, :hazard 20, :location, :name
   accepts_nested_attributes_for :hazards
   attr_accessible :hazards_attributes
   #accepts_nested_attributes_for :hazards, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
