2deb9e5392d564dd1631e4de5bf09c92583e8649
John Baycroft <jbaycroft@capaccio.com>
2013-11-20 13:06:32 -0500

    jsrev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,7 @@ RiskAssessDemo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( /dthmlx/dhtmlxcommon.js /dhtmlx/dhtmlxgrid.js /dhtmlx/dhtmlxgridcell.js )
-
+   config.assets.precompile += %w( *dhtmlx/* )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
